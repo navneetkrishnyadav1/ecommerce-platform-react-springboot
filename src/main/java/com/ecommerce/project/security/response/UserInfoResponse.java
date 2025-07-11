51c01165544bb15b02c609521b24a73b1a8b7bf0
Navneet Kumar <navneet.kumarbtech6418@gmail.com>
2025-07-12 03:30:00 +0530

    Fixed AuthEntryPointJwt for public authentication

---
+++ src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
@@ -0,0 +1,44 @@
+package com.ecommerce.project.security.response;
+
+import java.util.List;
+
+public class UserInfoResponse {
+    //once he is authenticated I want my request to return jwt token
+    //why jwt token because jwt token is like a ticket for the user which user can save his end and he can use for subsequent request
+    private Long id;
+    private String jwtToken;
+
+    private String username;
+    private List<String> roles;
+    public UserInfoResponse(Long id, String jwtToken, String username, List<String> roles) {
+        this.id = id;
+        this.jwtToken = jwtToken;
+        this.roles = roles;
+        this.username = username;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getUsername (){
+        return username;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
+    public String getJwtToken(){
+        return jwtToken;
+    }
+    public void setJwtToken(String jwtToken){
+        this.jwtToken = jwtToken;
+    }
+    public List<String> getRoles(){
+        return roles;
+    }
+    public void setRoles(List<String> roles){
+        this.roles = roles;
+    }
+}
